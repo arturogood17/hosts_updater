b08475f1998711b42e211850e4ff1aa744896223
arturogood17 <arturo_good17@hotmail.com>
2025-01-01 17:09:07 +0100

    Request.get() and test to see if everything was working as it should

--- hosts_updater.py
+++ hosts_updater.py
@@ -1,17 +1,16 @@
 import requests
-import shutil
 import os
 
 
 def main ():
     url = "https://a.dove.isdumb.one/list.txt"
-    with requests.get(url, stream= True) as r:
-        with open("update_to_host.txt", "wb") as file:
-            for chunk in r.iter_content(chunk_size= 8192):
+    with requests.get(url, stream= True) as r: 
+        with open("update_to_host.txt", "wb") as file: # cuando haces esto,
+            for chunk in r.iter_content(chunk_size= 8192): #se crea el archivo de una vez en la ruta donde tienes el proyecto
                 file.write(chunk)
 
-    with open("update_to_host.txt", "r", encoding="utf-8") as file:
-        contenido = file.read()
+    with open("update_to_host.txt", "r", encoding="utf-8") as file: #esto abre el archivo para que lo leas,
+        contenido = file.read()                                     #pero por alguna razón no lo muestra completo
         print(contenido)
     
 main()
