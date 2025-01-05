7280e7fc363f586b64cd004b1288104448c0002c
arturogood17 <arturo_good17@hotmail.com>
2025-01-06 00:03:18 +0100

    Minor changes

--- hosts_updater.py
+++ hosts_updater.py
@@ -1,5 +1,4 @@
 import requests
-import subprocess #Genera nuevos procesos. Sirve para ejecutar comandos del sistema operativo con Python.
 import shutil
 import os
 
@@ -21,44 +20,7 @@ def copy_to_path(path_origen, path_destino):
     if not os.path.isfile(path_destino):
         print(f"Copy failed. No se logró copiar el archivo {path_destino}")
     else:
-        print("Archivo copiado exitosamente a carpeta root.")
-
-def path_converter(path): #convierte la ruta a windows path
-    windows_path = subprocess.run(['wslpath', '-w', path], capture_output=True, text=True).stdout.strip() #-w convierte
-                                                                                                         # de WSL a Windows
-                                                                                                         #Usamos stdout para
-                                                                                                         #que solo de info
-                                                                                                         # del comando de salida
-                                                                                                         # no de errores
-    return windows_path    
-
-def final_copy_to_windows(w_path_origen, w_path_destino):
-    w_path_origen= w_path_origen.replace('\\', '\\\\').replace('"', '\\"')
-    w_path_destino= w_path_destino.replace('\\', '\\\\').replace('"', '\\"')
-    check_origin= f'powershell.exe Test-Path "{w_path_origen}"'
-    test_origin = subprocess.run(check_origin, capture_output=True, text=True, shell=True)
-    if test_origin.stdout.strip().lower() != "true":
-        print (f"{w_path_origen} not valid.")
-    try:
-        powershell_c = f'''if (Test-Path \'{w_path_destino}\') {{ Remove-Item \'{w_path_destino}\' }}; Copy-Item -Path \'{w_path_origen}\' -Destination \'{w_path_destino}\' -Force''' 
-        command= [
-            'powershell.exe',
-            'Start-Process',
-            'powershell',
-            '-Verb', 'RunAs',
-            '-ArgumentList',
-            f'-NoProfile -Command "{powershell_c}"'
-        ]
-        subprocess.run(command, shell=True, check=True)
-        check_final= f'powershell.exe Test-Path "{w_path_destino}"'
-        test_final = subprocess.run(check_final, capture_output=True, text=True, shell=True)
-        if test_final.stdout.strip().lower() != "true":
-            print (f"Copia unsuccessful.")
-        else:
-            print (f"Copia successful.")
-
-    except Exception as e:
-        return f"Error code: {e}"
+        print("Archivo copiado exitosamente.")
 
     
 def main ():
@@ -89,11 +51,7 @@ def main ():
             hosts.seek(pos) #vuelve al inicio del archivo
             hosts.truncate(pos)  # Esto limpia el archivo desde la posición hacia adelante
             hosts.writelines(updated) #escribe las líneas de nuevo con los cambios que hizo la función updating
-
-    final_hosts_path = "/mnt/c/Windows/System32/drivers/etc/hosts"
-
-    path_root_hosts= path_converter(path_hosts_destino)
-    final_hosts_path= path_converter(final_hosts_path) #convierte la ruta de estos archivos en rutas windows
-    final_copy_to_windows(path_root_hosts, final_hosts_path)
+    
+    copy_to_path(path_hosts_destino, path_hosts_desktop)
 
 main()
